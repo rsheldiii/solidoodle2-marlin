a64ffcbb983efae1ad18e3798a97fa491e2a4863
mlaws <mlaws@parra.catholic.edu.au>
2013-02-24 20:28:42 +1100

    Update Marlin/Configuration.h
    Fix date mistake.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -8,7 +8,7 @@
 //User specified version info of this build to display in [Pronterface, etc] terminal window during startup.
 //Implementation of an idea by Prof Braino to inform user that any changes made
 //to this build by the user have been successfully uploaded into firmware.
-#define STRING_VERSION_CONFIG_H __DATE__ "2013-02-24" __TIME__ // build date and time
+#define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
 #define STRING_CONFIG_H_AUTHOR "Lawsy" //Who made the changes.
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
